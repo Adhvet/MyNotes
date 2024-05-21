78ee5dda9e0aba753c715d78d1b981017c691971
Adhvet <sharmaadhvet@gmail.com>
2024-05-21 10:49:00 +0530

    step-9

--- lib/services/auth/firebase_auth_provider.dart
+++ lib/services/auth/firebase_auth_provider.dart
@@ -1,23 +1,11 @@
-import 'dart:ffi';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/services/auth/auth_user.dart';
-import 'package:mynotes/services/auth/auth_provider.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
 
-abstract class AuthProvider {
-  AuthUser? get currentuser;
-  Future<AuthUser> logIn({
-    required String email,
-    required String password,
-  });
-  Future<AuthUser> createUser({
-    required String email,
-    required String password,
-  });
-  Future<void> logOut();
-  Future<void> sendEmailVerification();
-}
+import 'package:firebase_auth/firebase_auth.dart'
+    show FirebaseAuth, FirebaseAuthException;
 
 class FirebaseAuthProvider implements AuthProvider {
   @override
@@ -105,5 +93,12 @@ class FirebaseAuthProvider implements AuthProvider {
     } catch (_) {
       throw GenericAuthException();
     }
+  }
+
+  @override
+  Future<void> initialize() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
 }
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,10 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:mynotes/constants/routes.dart';
-import 'package:mynotes/firebase_options.dart';
-import 'package:mynotes/views/login_view.dart';
-import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -28,13 +25,12 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
               "If you have'nt received an email yet, press the button below"),
           TextButton(
               onPressed: () async {
-                final user = FirebaseAuth.instance.currentUser;
-                await user?.sendEmailVerification();
+                await AuthService.firebase().sendEmailVerification();
               },
               child: const Text("Resend email verification")),
           TextButton(
             onPressed: () async {
-              await FirebaseAuth.instance.signOut();
+              await AuthService.firebase().logOut();
               Navigator.of(context).pushNamedAndRemoveUntil(
                 registerRoute,
                 (route) => false,
